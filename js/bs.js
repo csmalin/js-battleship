500044fbc8e54af18c4f0ad187437079ff60c793
Chris Malin <csmalin@gmail.com>
2013-05-23 16:07:56 -0700

    boards with coordinate logging on click

--- js/bs.js
+++ js/bs.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-	alert("yay");
+	$('td').on('click', function(e){
+		var cell = $(e)[0].target
+		var board = cell.parentElement.parentElement.parentElement.className;
+		var row = cell.parentElement.rowIndex;
+		var col = cell.cellIndex;
+		console.log(board + ": (" + row + "," + col + ")");
+
+		if (row > 0 && col > 0) {
+			console.log(cell);
+			$(cell).css("background-color", "#000")
+		};
+	});
 });
